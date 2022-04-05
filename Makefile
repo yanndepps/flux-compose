88eb189e5053d3d7d1a6e861bc0c6374f2f1e295
yanndepps <yann.deperrot@protonmail.com>
2022-04-05 23:39:45 +0200

    wip flux

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@
 CC=gcc
 
 # Tell the C compiler where to find <libguile.h>
-CFLAGS=`pkg-config --cflags guile-3.0`
+CFLAGS=`pkg-config --cflags guile-3.0 sdl2`
 
 # Tell the linker what libraries to use and where to find them.
-LIBS=`pkg-config --libs guile-3.0`
+LIBS=`pkg-config --libs guile-3.0 sdl2` -lpthread
 
 bin/flux-studio: bin/main.o
 	${CC} bin/main.o ${LIBS} -o bin/flux-studio
---
+++ examples/basic-gfx.scm
@@ -0,0 +1,14 @@
+;; Display context -> REPL or shell script
+
+;; Goal -> draw a red square on a white background
+(use-modules (flux graphics))
+
+(show-preview-window 1280 720)
+
+(define stream-bg '((width . 1000)
+                    (height . 1000)
+                    (instructions . ((set-fg-color 255 255 255 255)
+                                     (rect 0 0 1000 1000)
+                                     (set-fg-color 255 0 0 255)
+                                     (rect 100 100 800 800)))))
+(render-graphic stream-bg)
